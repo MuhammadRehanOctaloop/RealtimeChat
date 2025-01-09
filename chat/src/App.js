67f1bc560313f96e77ce7d47af43b4b2fe39ec03
MuhammadRehanOctaloop <muhammadrehanrasheedoctaloop@gmail.com>
2025-01-09 13:36:11 +0500

    Build

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUpPage from './Components/SignUpPage';
 import SignInPage from './Components/SignInPage';
 import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
